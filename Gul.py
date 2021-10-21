85a3c08828eee856c7912bfce1e96de37f3dba38
eCxb3 <89023586+eCxb3@users.noreply.github.com>
2021-10-21 17:13:26 +0300

    Update Gul.py

--- Gul.py
+++ Gul.py
@@ -7,10 +7,10 @@ async def ed(e):
     await e.edit("Я гуль")
     sleep(2)
     a = 1000
-    while a > -7:
+    while a > 0:
         c = a - 7
         await e.respond(str(a) + " - 7 = " + str(c))
         a = c
-    await e.respond("l l let me die haha")
+    await e.respond("l l let me die 1haha")
             
         
